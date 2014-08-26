53bbf728ac485d31734d2dcb9c3792cda228f47e
Brian Woodward <brian.woodward@gmail.com>
2014-08-26 12:38:48 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * vinyl-dat <https://github.com/doowb/vinyl-dat>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
